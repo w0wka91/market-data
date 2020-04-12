4a62c7568d17af139fb7f410b2545a2132151871
Waldemar <waldemar.penner91@gmail.com>
2020-04-12 14:30:16 +0200

    jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+podTemplate(yaml: """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: docker
+    image: docker
+    command: ['cat']
+    tty: true
+"""
+  ) {
+
+  node(POD_LABEL) {
+    def tag = "${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
+    def service = "market-data:${tag}"
+    checkout scm
+    stage('Build a Maven project') {
+      container('docker') {
+        sh("docker build -t ${service} .")
+      }
+    }
+  }
+}
